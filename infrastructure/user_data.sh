096a025cf0c9a2a8c97c275b47c110d20ce7676a
Brian Chew <brian@bchewy.com>
2024-03-15 02:43:39 +0800

    Update user_data.sh script to include docker-compose setup and database restore

--- infrastructure/user_data.sh
+++ infrastructure/user_data.sh
@@ -9,12 +9,23 @@ id ubuntu
 newgrp docker
 
 # get latest docker-compose (from s3; but ideally if the repo is pub we can just pull from the repo..)
+cd /home/ubuntu
 wget https://bchewy.s3.ap-southeast-1.amazonaws.com/docker-compose.yml
 wget -O dump.dump https://bchewy.s3.ap-southeast-1.amazonaws.com/Odoo+CRM+dump+Mar+14+2024.dump
 docker compose up --build -d
 
+# Rest till docker-compose is up; 5 mins
+sleep 360
+
 # Create postgres database, user and restore the dump
-docker exec -it ubuntu-db-1 psql -U odoo -c 'CREATE DATABASE odoo_actual;'
-docker exec -it ubuntu-db-1 psql -U odoo -c "CREATE ROLE odoo16 WITH LOGIN;"
-docker exec -i ubuntu-db-1 pg_restore -U odoo -d odoo_actual < dump.dump
+# docker exec -it ubuntu-db-1 psql -U odoo -c 'CREATE DATABASE odoo_actual;'
+# docker exec -it ubuntu-db-1 psql -U odoo -c "CREATE ROLE odoo16 WITH LOGIN;"
+# docker exec -i ubuntu-db-1 pg_restore -U odoo -d odoo_actual < dump.dump
+
+docker exec ubuntu-db-1 psql -U odoo -c 'CREATE DATABASE odoo_actual;'
+docker exec ubuntu-db-1 psql -U odoo -c "CREATE ROLE odoo16 WITH LOGIN;"
+cat dump.dump | docker exec -i ubuntu-db-1 pg_restore -U odoo -d odoo_actual
+
+# didnt work in 
+# docker exec ubuntu-db-1 pg_restore -U odoo -d odoo_actual < dump.dump
 
