9a9ddabf5d01cb978a9ac5fcaeeff16f8cfb7b5c
Brian Chew <brian@bchewy.com>
2024-03-15 02:05:01 +0800

    Update docker-compose.yml with new volumes

---
+++ infrastructure/user_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+
+# allow daemon access for other non-root users
+sudo usermod -a -G docker ubuntu
+id ubuntu
+newgrp docker
+
+# get latest docker-compose (from s3; but ideally if the repo is pub we can just pull from the repo..)
+wget https://bchewy.s3.ap-southeast-1.amazonaws.com/docker-compose.yml
+wget -O dump.dump https://bchewy.s3.ap-southeast-1.amazonaws.com/Odoo+CRM+dump+Mar+14+2024.dump
+docker compose up --build -d
+
+# Create postgres database, user and restore the dump
+docker exec -it ubuntu-db-1 psql -U odoo -c 'CREATE DATABASE odoo_actual;'
+docker exec -it ubuntu-db-1 psql -U odoo -c "CREATE ROLE odoo16 WITH LOGIN;"
+docker exec -i ubuntu-db-1 pg_restore -U odoo -d odoo_actual < dump.dump
+
