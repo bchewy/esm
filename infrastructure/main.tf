0d4b6116d995956aebbad852b28c4be565d529e9
Brian Chew <brian@bchewy.com>
2024-03-18 21:45:15 +0800

    Feat: Disabling tf state buckets - not possible with student account on Azure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,14 @@
+terraform {
+
+
+  # backend "azurerm" {
+  #   resource_group_name  = "tfstate"
+  #   storage_account_name = "tfstatebchewy"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
+
+}
 provider "azurerm" {
   features {
     resource_group {
@@ -7,6 +18,11 @@ provider "azurerm" {
   # Brian's Subscription ID
   subscription_id = "6d07e33b-f071-4121-9c74-7c575bafc191"
 }
+
+# Terraform items to store our tf state
+# module "states" {
+#   source = "./modules/states"
+# }
 
 module "dev" {
   source = "./modules/0-dev"
